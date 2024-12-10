b65c22f519a051940a75635059773a6053feb7bb
Курзенева Кристина <kristinakurzeneva35@gmail.com>
2024-12-10 11:32:33 +0500

    Добавьте файлы проекта.

---
+++ LB_3/FormMain.cs
@@ -0,0 +1,15 @@
+namespace LB_3
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+
+        }
+    }
+}
