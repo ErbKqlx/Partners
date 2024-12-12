fde79cdb2300b3db4353f8f4f883224758978195
Курзенева Кристина <kristinakurzeneva35@gmail.com>
2024-12-12 08:38:32 +0500

    rename

--- LB_3/FormMain.Designer.cs
+++ LB_3/FormMain.Designer.cs
@@ -37,7 +37,6 @@
             ClientSize = new Size(800, 450);
             Name = "FormMain";
             Text = "Партнеры";
-            Load += OnLoad;
             ResumeLayout(false);
         }
 
